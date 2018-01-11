c69af4e0fa229248589c9507f4ce74b7d11d1d97
brm <brm.ryd@gmail.com>
2018-01-11 08:17:47 +0700

    commit

---
+++ ansible/access-points2/cisco_scenarios_convert.py
@@ -0,0 +1,25 @@
+import yaml
+import re
+SCENARIO_FILE="scenarios/all.txt"
+GROUP_VAR_FILE="group_vars/all.yml"
+
+class ScenarioParser(object):
+
+    def __init__(self):
+        self.rc = 0
+        self.storage = dict()
+        self.file_content = dict()
+
+    def open(self):
+        try:
+            with open(GROUP_VAR_FILE, 'r') as fileObj:
+                self.file_content=yaml.load(fileObj)
+        except:
+            open(GROUP_VAR_FILE, 'w').close()
+
+    def read(self):
+        scenario_number = 0
+        scenario_step = 0
+        scenario_name = ''
+        name_pattern = re.compile(r'^(\d+)\.?\s+(.*)')
+        step_pattern = re.compile(r'.*[])
