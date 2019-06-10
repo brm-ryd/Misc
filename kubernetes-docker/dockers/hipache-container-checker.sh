ebb3c71379a87ac06e899da775fac00a4ee92df6
brm <brm.ryd@gmail.com>
2019-06-10 19:49:59 +0700

    commit

--- kubernetes-docker/dockers/hipache-container-checker.sh
+++ kubernetes-docker/dockers/hipache-container-checker.sh
@@ -26,6 +26,20 @@ echo $REMOTE_VERSION > $VERSION_FILE
 #launch new one cont,,,
 # echo 
 # set timeout
+echo "$(date + "%Y-%m-%d %H:%M:%S %Z") launching $DOCKER_IMAGE_NAME, logging to $LOG_DIR"
+mkdir -p $LOG_DIR
+NEW_WEBAPP_ID="abcdefghijklmnopqrstuvwxyz"
+MAX_TIMEOUT=5
+set +e
+until [ $MAX_TIMEOUT -le 0 ] || NEW_WEBAPP_ID=$(docker run -P -h $(hostname) --link hipache:hipache $(dockerParameters $BRANCH) -d -v $LOG_DIR:/logs $DOCKER_IMAGE_NAME); do
+       echo -n "."
+       sleep 1
+       let MAX_TIMEOUT -= 1
+done
+set -e
+
 
 # check if web app inside container running
 #
+#NEW_WEBAPP_IP_ADDR=will inspect network setting ip address docker
+#check new docker and deploy if its not running
