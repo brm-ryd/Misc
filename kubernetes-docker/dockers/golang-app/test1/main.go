abf10f95a93c3608eea3320a7dfa1a941c59e235
brm <brm.ryd@gmail.com>
2019-07-30 12:25:13 +0700

    commit

--- kubernetes-docker/dockers/golang-app/test1/main.go
+++ kubernetes-docker/dockers/golang-app/test1/main.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func handler (w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "test,")
 }
 
 /* func reqreceiver() {
-	will try to handle packet traffic receive 
+	will try to handle packet traffic receive
 } */
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.data(http.ListenAndServe(":8888", nil))
-}
-
-func readnet() {
-	int i := 0;
-	var x := 
+	// log.data(http.ListenAndServe(":8888", nil))
 }
